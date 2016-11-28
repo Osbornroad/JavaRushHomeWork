4e90ea4f6b1c2aab7f7f4c640f58ece846119ea1
Maksim Tkachenko <osbornroad@gmail.com>
2016-11-28 22:00:48 +0400

    level32.lesson08.home01.Ready

--- src/com/javarush/test/level32/lesson08/home01/CustomInvocationHandler.java
+++ src/com/javarush/test/level32/lesson08/home01/CustomInvocationHandler.java
@@ -22,6 +22,8 @@ public class CustomInvocationHandler implements InvocationHandler
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
     {
         System.out.println(method.getName() + " in");
+        method.invoke(someInterfaceWithMethods, args);
+        System.out.println(method.getName() + " out");
         return null;
     }
 }
